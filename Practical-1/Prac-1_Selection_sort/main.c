38c8df479500fdab1d24bf6d64933616825877a0
VishwajeetMuthe1970 <93135572+VishwajeetMuthe1970@users.noreply.github.com>
2023-08-01 15:37:50 +0530

    Add files via upload

--- Practical-1/Prac-1_Selection_sort/main.c
+++ Practical-1/Prac-1_Selection_sort/main.c
@@ -1,30 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<conio.h>
 void main()
 {
-  int a[20], min,i,j,n, temp=0;
-  printf("Please enter the size of array: ");
-  scanf("%d", &n);
+	int a[20], i, j, temp, n, min;
+	printf("Enter The Value of n : ");
+	scanf("%d",&n);
 
-for(i=0;i<n;i++){
-scanf("%d", &a[i]);
-}
-for(i=0;i<=n-2;i++){
-    for(j=i+1; j<=n-1;j++){
-        if(a[j]<a[min]){
-            min = j;
-        }
-     }
-        temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
-}
+	for(i=0;i<n;i++)
+	{
+		printf("Enter a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
 
-  for(i=0;i<n;i++){
-    printf("%d\t",a[i]);
-  }
-}
+	for(i=0;i<=n-2;i++)
+	{
+		min = i;
+		for(j=i+1;j<=n-1;j++)
+		{
+			if(a[j]<a[min])
+			{
+				min = j;
+			}
+		}
+		temp = a[i];
+		a[i] = a[min];
+		a[min] = temp;
+	}
+	printf("Sorted array is : ");
+	for(i=0;i<n;i++){
 
+		printf(" %d ",a[i]);
+	}
+	getch();
+}
 /*
 #include <stdio.h>
 #include<stdlib.h>
